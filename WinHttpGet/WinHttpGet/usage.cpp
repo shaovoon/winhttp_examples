c01ea7c0061b205ba9f9fd4bdf55220b5935783c
shaovoon <shaovoon@yahoo.com>
2020-09-19 13:53:53 +0800

    Remove the WebApplication1

--- WinHttpGet/WinHttpGet/usage.cpp
+++ WinHttpGet/WinHttpGet/usage.cpp
@@ -4,36 +4,6 @@
 #include <iostream>
 #include "WinHttpWrapper.h"
 
-/*
-int main()
-{
-	std::wstring domain = L"localhost";
-	std::wstring rest_of_path = L"/Default?SearchTerm=Hello";
-	int port = 54170;
-	std::string text;
-	std::wstring requestHeader;
-
-	using namespace WinHttpWrapper;
-	HttpResponse response;
-
-	HttpRequest req(domain, port, false);
-	bool success = req.Get(
-		rest_of_path, requestHeader,
-		response);
-	if (success)
-	{
-		std::cout << "StatusCode:" << response.statusCode << std::endl;
-		std::cout << "Output:" << response.text << std::endl;
-		std::wcout << L"ResponseHeader:" << response.header << std::endl;
-	}
-	else
-	{
-		std::cout << "StatusCode:" << response.statusCode << std::endl;
-		std::wcout << "Error:" << response.error << std::endl;
-	}
-}
-*/
-
 int main()
 {
 	std::wstring domain = L"localhost";
